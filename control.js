02c50461ef13eb0883a29f8b22763b8f0d644fa2
Alan Elam <aelam757@gmail.com>
2020-01-09 01:24:48 -0500

    still no working

--- control.js
+++ control.js
@@ -1,212 +1,208 @@
-var startButton = document.getElementById('startButton')
-var questionText = document.getElementById('questionText')
-var printScore = document.getElementById('score')
-var scoreForm = document.getElementById('scoreForm')
-var btn0 = document.getElementById('btn0')
-var btn1 = document.getElementById('btn1')
-var btn2 = document.getElementById('btn2')
-var btn3 = document.getElementById('btn3')
-
-
-var highScoreList = document.getElementById("high-score");
-
-var scoreInput = document.getElementById('formGroupExampleInput')
-var timer = document.getElementById("timer")
-
-
-
-
-var scoreArr = ["Anikan Skywalker: 200", "Hon Solo: 199"]
-
-var gameOverSwitch = 0;
-var questionNumber = 0;
-var score = 0;
-var countdown = 60;
-function setTime() {
-    var timerInterval = setInterval(
-        function () {
-            countdown--;
-            timer.innerText = countdown + " seconds left";
-            if (countdown === 0) {
-                clearInterval(timerInterval)
-                timer.textContent = ""
-                gameOver();
-                alert("Out of Time!")
-            }
-            if (gameOverSwitch === 1) {
-                clearInterval(timerInterval)
-                timer.textContent = ""
-            }
-        }, 1500);
-}
-
-var questions = [
-    {
-        title: "What is the language that describes the style of an HTML document?",
-        choices: ["CSS", "AJAX", "PYTHON", "SQL"],
-        answer: "CSS"
-    },
-    {
-        title: "What is the programming language of HTML and the Web?",
-        choices: ["Bootstrap", "HTML", "CSS", "JavaScript"],
-        answer: "JavaScript"
-    },
-    {
-        title: "What is the most popular HTML,CSS, and Javascript framework?",
-        choices: ["jQuery", "Bootstrap", "PHP", "SQ"],
-        answer: "Bootstrap"
-    },
-    {
-        title: "What greatly simplifies Javascript programming?",
-        choices: ["HTML", "CSS", "jQuery", "Bootstrap"],
-        answer: "jQuery"
-    },
-    {
-        title: "What is the worlds largest web developer site?",
-        choices: ["w3schools", "Stack Overflow", "GitLab", "Github"],
-        answer: "w3schools"
-    },
+var questions = [{
+    question: "1.What is the language that describes the style of an HTML document?",
+    choices: ["CSS", "AJAX", "PYTHON", "SQL"],
+    answer: 0
+},
+{
+    question: "2.What is the programming language of HTML and the Web?",
+    choices: ["Bootstrap", "HTML", "CSS", "JavaScript"],
+    answer: 3
+},
+{
+    question: "3.What is the most popular HTML,CSS, and Javascript framework?",
+    choices: ["jQuery", "Bootstrap", "PHP", "SQ"],
+    answer: 1
+},
+{
+    question: "4.What greatly simplifies Javascript programming?",
+    choices: ["HTML", "CSS", "jQuery", "Bootstrap"],
+    answer: 2
+},
+{
+    question: "5.What is the worlds largest web developer site?",
+    choices: ["w3schools", "Stack Overflow", "GitLab", "Github"],
+    answer: 0
+},
 ];
 
-initialize(); {
+let currentQuestion = 0;
+let viewingAn = 0;
+let correctAnswer = 0;
+let quizOver = false;
+let SelectedAnswer = [];
+let c = 195;
+let t;
 
-    startButton.setAttribute("style", "display: none")
-    questionText.classList.remove('hide')
-    loadQuestion();
-    setTime();
-    printScore.innerText = "Score: " + score
-};
+$(document).ready(function () {
 
-function loadQuestion() {
-    gameOverSwitch = 0;
-    if (questionNumber < questions.length) {
-        questionText.innerText = questions[questionNumber].title;
-        btn0.innerText = questions[questionNumber].choices0;
-        btn1.innerText = questions[questionNumber].choices1;
-        btn2.innerText = questions[questionNumber].choices2;
-        btn3.innerText = questions[questionNumber].choices3;
+    //Question Display
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+
+    timedCount();
 
 
-        $("#btn0").on('click', function () {
-            if (btn0.innerText === questions[questionNumber].answer) {
-                console.log("questions number: " + questionNumber)
-                console.log("Correct")
-                score += 20;
-                printScore.innerText = "Score: " + score
-            } else {
-                console.log("Incorrect")
-                countdown -= 10
-                printScore.innerText = "Score: " + score
-            } loadNextQuestion();
-        });
+    if (!quizOver) {
+        if (currentQuestion == 0) { return false; }
 
-
-        $("#btn1").on('click', function () {
-            if (btn1.innerText === questions[questionNumber].answer) {
-                score += 20;
-                printScore.innerText = "Score: " + score
-                console.log("Correct")
-            } else {
-                console.log("Incorrect")
-                countdown -= 10
-                printScore.innerText = "Score: " + score
-            } loadNextQuestion();
-        });
-
-
-        $("#btn2").on('click', function () {
-            if (btn2.innerText === questions[questionNumber].answer) {
-                score += 20;
-                printScore.innerText = "Score: " + score
-                console.log("Correct")
-            } else {
-                console.log("Incorrect")
-                countdown -= 10
-                printScore.innerText = "Score: " + score
-            } loadNextQuestion();
-        });
-
-        $("#btn3").on('click', function () {
-            if (btn3.innerText === questions[questionNumber].answer) {
-                score += 20;
-                console.log("Correct")
-                printScore.innerText = "Score: " + score
-            } else {
-                console.log("Incorrect")
-                countdown -= 10
-                printScore.innerText = "Score: " + score
-            } loadNextQuestion();
-        });
-    } else {
-    }
-}
-
-function loadNextQuestion() {
-    questionNumber++;
-    console.log(questionNumber)
-    if (questionNumber < questions.length) {
-        printScore.innerText = "Score: " + score
-        questionText.innerText = questions[questionNumber].title;
-        btn0.innerText = questions[questionNumber].choices0;
-        btn1.innerText = questions[questionNumber].choices1;
-        btn2.innerText = questions[questionNumber].choices2;
-        btn3.innerText = questions[questionNumber].choices3;
-    } else {
-        console.log("Game Over")
-        questionText.classList.add('hide')
-        gameOver();
-    }
-}
-
-function gameOver() {
-    timer.textContent = ""
-    gameOverSwitch++;
-    questionText.classList.remove('hide')
-    questionText.innerText = "High Score"
-    scoreForm.removeAttribute("class", "hide")
-    scoreForm.addEventListener('submit', function (event) {
-        event.preventDefault()
-
-
-        renderScore()
-    });
-}
-
-function renderScore() {
-    var highScoreList = innerText = "";
-    var highScoreName = scoreInput.value.trim();
-    var highScoreData = (highScoreName + " : " + score);
-    if (gameOverSwitch > 0) {
-
-
-        if (score > 0) {
-            scoreArr.push(highScoreData)
-        } else {
-            alert("A score of 0 will not be recorded")
+        currentQuestion--;
+        if (currentQuestion < questions.length) {
+            displayCurrentQuestion();
         }
+    } else {
+        if (viewAns == 0) { return false; }
+        currentQuestion = 0; viewingAn = 0;
+        viewResults();
+    }
+});
+
+$(this).find(".nextBtn").on("click", function () {
+    if (!quizOver) {
+        let val = $("input[type = 'radio']: checked").val();
+
+        if (val == undefined) {
+            $(document).find(".quizMessage").text("Please select a answer");
+            $(document).find(".quizMessage").show();
+        }
+        else {
+            //Remove message, not sure if I need to call this each time.
+            $(dcoument).find(".quizMessage").hide();
+            if (val == questions[currentQuestion].correctAnswer) {
+                correctAnswer++;
+            }
+            SelectedAnswer[currentQuestion] = val;
+
+            currentQuestion++;
+            if (currentQuestion >= 1) {
+            }
+            if (currentQuestion < questions.length) {
+                displayCurrentQuestion();
+            }
+            else {
+                displayScore();
+                $('#Timeshown').html('Time is UP!');
+                $('#timer').html("Your Score: " + correctAnswer + "Out of:" + questions.length);
+                c = 200;
+                $(document).find(".nextBtn").text("Play Again?");
+                quizOver = true;
+                return false;
+            }
+
+        }
+
+    }
+    else {
+        // Next button will restart the quiz("Play agian?")
+        quizOver = false; $('#TimeShown').html('TimeRemaing:'); SelectedAnswer = [];
+        $(document).find(".nextBtn").text("Next Question");
+        resetQuiz();
+        viewingAn = 0;
+        displayCurrentQuestion();
+        hide();
+    }
+});
+
+
+function timeCount() {
+    if (c == 200) {
+        return false;
     }
 
+    let minutes = parseInt(c / 60) % 50;
+    let seconds = c % 60;
+    let result = (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
+    $("#timer").html(result);
 
-    for (i = 0; i < scoreArr.length; i++) {
-        var scoreTest = scoreArr[i]
-        var li = document.createElement("li")
-        li.textContent = scoreTest
-        highScoreList.append(li)
+    if (c == 0) {
+        displayScore();
+        $('#TimeShown').html('Time is UP!');
+        $('#timer').html("You scored: " + correctAnswer + "Out of: " + questions.length);
+        c = 195
+        $(document).find(".nextBtn").text("Play Again?");
+        quizOver = true;
+        return false;
     }
-
-    var dataStorage = JSON.stringify(scoreArr)
-    localStorage.setItem("userData", dataStorage)
-    console.log(dataStorage);
 }
 
-function initialize() {
-    var storedString = localStorage.getItem("userData")
-    var parseString = JSON.parse(storedString)
-    if (parseString !== null) {
-        console.log(parseString)
-        scoreArr = parseString;
+if (c == 0) {
+    if (!quizOver) {
+        let val = $("input[type= 'radio]:checked").val();
+        if (val == questions[currentQuestion].correctAnswer) {
+            correctAnswers++;
+        }
+        currentQuestion++;
+
+        if (currentQuestion < questions.length) {
+            displayCurrentQuestion();
+            c = 20;
+        }
+        else {
+            displayScore();
+            $('#timer').html('');
+            c = 25;
+            $(document).find(".nextBtn").text("Play Again?");
+            quizOver = true;
+            return false;
+        }
 
     }
-    renderScore();
+    else {
+        quizOver = false;
+        $(document).find(".nextBtn").text("Next Question");
+        resetQuiz();
+        displayCurrentQuestion();
+        hideScore();
+    }
+}
+c = c - 1;
+t = setTimeout(function () {
+    timedCount()
+}, 1000);
 
+
+// This is the function for the quetions and choices
+function displayCurrentQuestion() {
+    if (c == 200) { c = 195; timedCount(); }
+    let question = questions[currentQuestion].question;
+    let questionClass = $(document).find(".grid > .question");
+    let choiceList = $(document).find(".grid > .question");
+    let numbChoices = questions[currentQuestion].choices.length;
+    $(questionClass).text(question);
+    let choice;
+}
+
+function resetQuiz();
+{
+    currentQuestion = 0;
+    correctAnswer = 0;
+    hideScore();
+}
+
+function displayScore() {
+    $(document).find(".grid > .answer").text("Your Score: " + correctAnswer + "Out of: " + questions.length);
+    $(document).find(".grid > .answer").show();
+}
+
+function hideScore() {
+    $(dcoument).find(".answer").hide();
+}
+
+// Display the current question and choices
+function viewResults() {
+    if (currentQuestion == 20) { currentQuestion = 0; return false; }
+    if (viewingAn == 1) { return false; }
+
+    hideScore();
+    let question = questions[currentQuestion].question;
+    let questionClass = $(document).find(".grid > .question");
+    let choicesList = $(document).find(".grid > .choiceList");
+    let numbChoices = questions[currentQuestion].choices.length;
+    $(questionClass).text(question);
+    let choice;
+
+    currentQuestion++;
+
+    setTimeout(function () {
+        viewResults();
+    }, 5000);
 }
