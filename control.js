293a3b1b6b5678f0fb7d637b750d31b237259905
Alan Elam <aelam757@gmail.com>
2020-01-30 18:16:43 -0500

    still working on js

--- control.js
+++ control.js
@@ -1,19 +1,16 @@
 let currentQuestion = 0;
-let viewingAn = 0;
-let correctAnswer = 0;
+let score = 0;
 let quizOver = false;
-let c = 195;
-let t;
-let choicesList = $(this).siblings("#choiceList");
-let questionClass = $(this).siblings("#question");
-let answer = $(this).siblings(".answer");
-let timer = document.getElementById("timer")
-
+let choicesList = $("#choiceList");
+let answer = $(".answer");
+let timer = $("timer")
+//let totalQuestions = questions.length;
+//let questions = 0;
 
 $(document).ready(function () {
 
     let questions = [{
-    
+
         question: "1.What is the language that describes the style of an HTML document?",
         choices: ["CSS", "AJAX", "PYTHON", "SQL"],
         answer: 0
@@ -39,49 +36,66 @@ $(document).ready(function () {
         answer: 0
     },
     ];
+ //   for (var i = 0; i < questions.length; i++) {
+   //     var response = window.prompt(questions[i].question);
+    //    if (response == questions[i].answer)
+     //       score++;
+     //   alert("Correct!");
+    //} else {
+    //    alert("Sorry that was incorrect!");
+    //}
     
 
     displayCurrentQuestion();
-    //$(this).find(".question");
     resetQuiz();
     displayScore();
     displaychoicesList();
     displayingAnswer();
-    
+    loadNextQuestion();
+
     function displayCurrentQuestion() {
         $("#question").text(questions[currentQuestion].question);
         console.log(questions)
     }
     console.log(questions[currentQuestion].choices[0])
-   
-    function displaychoicesList(){
-        $("#choiceList").text(choiceList[currentQuestion],".answer");
+
+    function displaychoicesList() {
+        $("#choiceList").text(choiceList[currentQuestion], ".answer");
+        for(var i = 0; i < question.length; i++){
+            var response = window.questions(questions[i].question);
+            if (response == question[i].answer)
+            score++;
+            alert("Correct!");
+        } else{
+            alert("Sorry, that was incorrect!");
+        }
         console.log(choicesList)
         console.log(answer)
     }
-    function displayingAnswer(){
+    function displayingAnswer() {
         $(".btn0").text(questions[currentQuestion].choices[0]);
         $(".btn1").text(questions[currentQuestion].choices[1])
         $(".btn2").text(questions[currentQuestion].choices[2])
         $(".btn3").text(questions[currentQuestion].choices[3])
     }
-    
-    
-    function resetQuiz()
-    {
+    function loadNextQuestion() {
+
+    }
+
+    function resetQuiz() {
         currentQuestion = 0;
         correctAnswer = 0;
         hideScore();
     }
-    
+
     function displayScore() {
         $(document).find(".grid > .answer").text("Your Score: " + correctAnswer + "Out of: " + questions.length);
         $(document).find(".grid > .answer").show();
     }
-    
+
     function hideScore() {
         $(document).find(".answer");
-    }if (!quizOver) {
+    } if (!quizOver) {
         if (currentQuestion == 0) { return false; }
 
         currentQuestion--;
@@ -96,8 +110,7 @@ $(document).ready(function () {
 });
 
 
-function resetQuiz()
-{
+function resetQuiz() {
     currentQuestion = 0;
     correctAnswer = 0;
     hideScore();
